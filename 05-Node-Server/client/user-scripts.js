d612dcbe9853bcd823583707306f3999ee19ee8f
RaheemMalik317 <82741663+RaheemMalik317@users.noreply.github.com>
2021-08-02 11:48:03 -0400

    logout complete

--- 05-Node-Server/client/user-scripts.js
+++ 05-Node-Server/client/user-scripts.js
@@ -31,13 +31,40 @@ function userSignUp() {
 //USER LOGIN
 
 function userLogin() {
-    console.log('userLogin Function Called')
+    //console.log('userLogin Function Called')
+    let userEmail = document.getElementById('emailLogin').value;
+    let userPass = document.getElementById('pwdLogin').value;
+    console.log(userEmail, userPass);
+
+    let userData = {user: {email: userEmail, password: userPass}};
+    console.log(`USERDATA ==> ${userData.user.email} ${userData.user.password}`);
+
+    fetch('http://localhost:3000/user/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    })
+    .then(response => response.json())
+    .then(function(response) {
+        console.log(response.sessionToken);
+        let token = response.sessionToken;
+        localStorage.setItem('SessionToken', token);
+        tokenChecker()
+    })
+    .catch((err) => {
+        console.log(e);
+    })
 }
 
 //USER LOGOUT
 
 function userLogout() {
-    console.log('userLogout Function Called')
+    //console.log('userLogout Function Called')
+    localStorage.setItem('SessionToken', undefined)
+    console.log(`sessionToken ==> ${localStorage.sessionToken}`);
+    tokenChecker()
 }
 
 //TOKEN CHECKER FUNCTION
--- 05-Node-Server/server/app.js
+++ 05-Node-Server/server/app.js
@@ -9,11 +9,12 @@ let journal = require('./controllers/journalcontroller');
 
 sequelize.sync();
 //sequelize.sync({force: true})
+app.use(require('./middleware/headers'))
 app.use(express.json())
 app.use('/journal', journal)
 app.use('/user', user)
 // app.use(require('./middleware/validate-session'))
 
 app.listen(3000, function() {
-        console.log('App is listening on port 3001');
+        console.log('App is listening on port 3000');
 })
